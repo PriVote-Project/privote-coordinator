638c70ef236820866028f7ef4d58fb5706622382
silent-cipher <puspendramahariya@gmail.com>
2025-07-18 03:13:51 +0530

    subgraph service cleanup

--- ts/subgraph/subgraph.service.ts
+++ ts/subgraph/subgraph.service.ts
@@ -1,7 +1,30 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 
 /**
  * SubgraphService is responsible for deploying subgraph.
  */
 @Injectable()
-export class SubgraphService {}
+export class SubgraphService {
+  /**
+   * Logger
+   */
+  private readonly logger: Logger;
+
+  /**
+   * Subgraph endpoint
+   */
+  private readonly endpoint: string;
+
+  constructor() {
+    this.logger = new Logger(SubgraphService.name);
+    this.endpoint = process.env.SUBGRAPH_ENDPOINT!;
+  }
+
+  /**
+   * Fetch all Coordinators
+   */
+  fetchAllCoordinators(): void {
+    this.logger.error("fetchAllCoordinators: Not Implemented yet!", { endpoint: this.endpoint });
+    throw new Error("fetchAllCoordinators: Not Implemented yet!");
+  }
+}
