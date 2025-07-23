8c48a99120ee13cb20efaf1294c9d0d5d9fb801b
silent-cipher <puspendramahariya@gmail.com>
2025-07-23 23:48:52 +0530

    sync with maci coordinator

--- ts/app.module.ts
+++ ts/app.module.ts
@@ -4,7 +4,9 @@ import { ThrottlerModule } from "@nestjs/throttler";
 import { CryptoModule } from "./crypto/crypto.module";
 import { DeployerModule } from "./deployer/deployer.module";
 import { FileModule } from "./file/file.module";
+import { HealthModule } from "./health/health.module";
 import { ProofModule } from "./proof/proof.module";
+import { RedisModule } from "./redis/redis.module";
 import { SessionKeysModule } from "./sessionKeys/sessionKeys.module";
 import { SubgraphModule } from "./subgraph/subgraph.module";
 
@@ -16,12 +18,14 @@ import { SubgraphModule } from "./subgraph/subgraph.module";
         limit: Number(process.env.LIMIT),
       },
     ]),
+    HealthModule,
     FileModule,
     CryptoModule,
     ProofModule,
     SessionKeysModule,
     DeployerModule,
     SubgraphModule,
+    RedisModule,
   ],
 })
 export class AppModule {}
